d8cd86a63c2665db249c424da29aa24ce2e245cd
zinntikumugai <uesitananame55@gmail.com>
2025-02-10 01:23:37 +0900

    oled support

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,26 @@
 use defmt::*;
 use defmt_rtt as _;
 use panic_probe as _;
-use rp2040_hal as hal;
+use rp2040_hal::{self as hal, fugit::RateExtU32};
 
 use hal::pac;
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+    primitives::{PrimitiveStyleBuilder, Rectangle, Triangle, Circle},
+};
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 
-use embedded_hal::delay::DelayNs;
-use embedded_hal::digital::OutputPin;
 
 #[link_section = ".boot2"]
 #[used]
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
 
 const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+
 
 #[rp2040_hal::entry]
 fn main() -> ! {
@@ -36,8 +44,6 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let mut timer = rp2040_hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
-
     let sio = hal::Sio::new(pac.SIO);
 
     let pins = hal::gpio::Pins::new(
@@ -47,28 +53,152 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    let mut green_led = pins.gpio22.into_push_pull_output();
-    let mut orange_led = pins.gpio21.into_push_pull_output();
-    let mut red_led = pins.gpio20.into_push_pull_output();
+
+    let sda_pin_a = pins.gpio16.into_pull_up_input().into_function::<hal::gpio::FunctionI2C>();
+    let scl_pin_a = pins.gpio17.into_pull_up_input().into_function::<hal::gpio::FunctionI2C>();
+
+    let sda_pin_b = pins.gpio18.into_pull_up_input().into_function::<hal::gpio::FunctionI2C>();
+    let scl_pin_b = pins.gpio19.into_pull_up_input().into_function::<hal::gpio::FunctionI2C>();
+    
+    // I2C0 の初期化（400kHz）
+    let i2c0 = hal::I2C::i2c0(
+        pac.I2C0,
+        sda_pin_a,
+        scl_pin_a,
+        400u32.kHz(),
+        &mut pac.RESETS,
+        &clocks.system_clock,
+    );
+
+    let interface_a = I2CDisplayInterface::new(i2c0);
+    let mut display_a = Ssd1306::new(
+        interface_a,
+        DisplaySize128x64,
+        DisplayRotation::Rotate0,
+    ).into_buffered_graphics_mode();
+    display_a.init().unwrap();
+    
+    // I2C1の初期化
+    let i2c1 = hal::I2C::i2c1(
+        pac.I2C1,
+        sda_pin_b,
+        scl_pin_b,
+        400u32.kHz(),
+        &mut pac.RESETS,
+        &clocks.system_clock,
+    );
+    let interface_b = I2CDisplayInterface::new(i2c1);
+    let mut display_b = Ssd1306::new(
+        interface_b,
+        DisplaySize128x32,
+        DisplayRotation::Rotate0,
+    ).into_buffered_graphics_mode();
+    display_b.init().unwrap();
+    
+    let yoffset = 8;
+
+    let style = PrimitiveStyleBuilder::new()
+        .stroke_width(1)
+        .stroke_color(BinaryColor::On)
+        .build();
+    
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+
+    // display A
+    Text::with_baseline("Hello world!", Point::new(0, 32), text_style, Baseline::Top)
+        .draw(&mut display_a)
+        .unwrap();
+
+    Text::with_baseline("Hello Rust!", Point::new(0, 40), text_style, Baseline::Top)
+        .draw(&mut display_a)
+        .unwrap();
+
+    Text::with_baseline("zinntikumugai.com", Point::new(0, 48), text_style, Baseline::Top)
+        .draw(&mut display_a)
+        .unwrap();
+    
+    // screen outline
+    // default display size is 128x64 if you don't pass a _DisplaySize_
+    // enum to the _Builder_ struct
+    Rectangle::new(Point::new(0, 0), Size::new(127, 31))
+        .into_styled(style)
+        .draw(&mut display_a)
+        .unwrap();
+
+    // triangle
+    Triangle::new(
+        Point::new(16, 16 + yoffset),
+        Point::new(16 + 16, 16 + yoffset),
+        Point::new(16 + 8, yoffset),
+    )
+    .into_styled(style)
+    .draw(&mut display_a)
+    .unwrap();
+
+    // square
+    Rectangle::new(Point::new(52, yoffset), Size::new_equal(16))
+        .into_styled(style)
+        .draw(&mut display_a)
+        .unwrap();
+
+    // circle
+    Circle::new(Point::new(88, yoffset), 16)
+        .into_styled(style)
+        .draw(&mut display_a)
+        .unwrap();
+
+    // 
+
+    display_a.flush().unwrap();
+
+    // Display b
+    Text::with_baseline("Hello world!", Point::new(0, 32), text_style, Baseline::Top)
+        .draw(&mut display_b)
+        .unwrap();
+
+    Text::with_baseline("Hello Rust!", Point::new(0, 40), text_style, Baseline::Top)
+        .draw(&mut display_b)
+        .unwrap();
+    
+    // screen outline
+    // default display size is 128x64 if you don't pass a _DisplaySize_
+    // enum to the _Builder_ struct
+    Rectangle::new(Point::new(0, 0), Size::new(127, 31))
+        .into_styled(style)
+        .draw(&mut display_b)
+        .unwrap();
+
+    // triangle
+    Triangle::new(
+        Point::new(16, 16 + yoffset),
+        Point::new(16 + 16, 16 + yoffset),
+        Point::new(16 + 8, yoffset),
+    )
+    .into_styled(style)
+    .draw(&mut display_b)
+    .unwrap();
+
+    // square
+    Rectangle::new(Point::new(52, yoffset), Size::new_equal(16))
+        .into_styled(style)
+        .draw(&mut display_b)
+        .unwrap();
+
+    // circle
+    Circle::new(Point::new(88, yoffset), 16)
+        .into_styled(style)
+        .draw(&mut display_b)
+        .unwrap();
+
+    // 
+
+    display_b.flush().unwrap();
+
 
     loop {
-        info!("green");
-        green_led.set_high().unwrap();
-        timer.delay_ms(2000);
-        green_led.set_low().unwrap();
-
-        info!("orange");
-        for _ in 1..4 {
-            orange_led.set_high().unwrap();
-            timer.delay_ms(500);
-            orange_led.set_low().unwrap();
-            timer.delay_ms(500);
-        }
-        orange_led.set_low().unwrap();
-
-        info!("red");
-        red_led.set_high().unwrap();
-        timer.delay_ms(2000);
-        red_led.set_low().unwrap();
     }
 }
